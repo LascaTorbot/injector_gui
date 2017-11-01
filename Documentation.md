bd8bcfa6b9f35e6ba3c4eb1b05b5d729eccf5c3e
André Almeida <fda.andre@gmail.com>
2017-11-01 16:37:38 -0200

    Implement thread on module_run

---
+++ Documentation.md
@@ -0,0 +1,3 @@
+# Documentation
+
+This software is separeted in differents files. The `injector_gui` is the "main" file. It's call the GUI up and the main functions. In `injector_gui`, the main window is created, so as the main table and buttons to call others modules/functions.
--- module_run_script.py
+++ module_run_script.py
@@ -22,7 +22,8 @@ def verifyRaw(pkt, substring):
 
 # Return True if pkt contains the substring
 def contains(substring):
-        sniff(filter="tcp", prn=lambda pkt: verifyRaw(pkt, substring))
+        sniff(filter="tcp", stop_filter = lambda pkt: verifyRaw(pkt, substring))
+        return True
 
 
 # Return true if the source IP is hostname
@@ -51,13 +52,26 @@ def setTextBox(log, original, new):
     original_textBox = original
     new_textBox = new
 
-class Module_Run_Script(QThread):
-    def __init__(self):
+class Run_Script(QThread):
+    def __init__(self, script):
         QThread.__init__(self)
-        self.script = ""
+        self.script = script
 
     def __del__(self):
         self.wait()
+
+    def run(self):
+        #print("im running")
+        try:
+            exec("if " + self.script)
+        except:
+            print("Wrong syntax!")
+        #print("finished")
+        #print("if " + self.script)
+
+class Module_Run_Script(QThread):
+    def __init__(self):
+        QThread.__init__(self)
 
     def setupUI(self):
         window = QWidget()
@@ -71,9 +85,9 @@ class Module_Run_Script(QThread):
         window_layout.addWidget(btn_load, 0, 0)
         window_layout.addWidget(lbl_loaded, 0, 1)
 
-        script_textBox = TextBox()
-        window_layout.addWidget(script_textBox, 1, 0, 1, 0)
-        self.highlight = PythonHighlighter(script_textBox.document())
+        self.script_textBox = TextBox()
+        window_layout.addWidget(self.script_textBox, 1, 0, 1, 0)
+        self.highlight = PythonHighlighter(self.script_textBox.document())
 
         btn_att = QPushButton("Attack!")
         window_layout.addWidget(btn_att, 2, 0)
@@ -82,23 +96,24 @@ class Module_Run_Script(QThread):
         def on_click():
             self.attack_file = QFileDialog.getOpenFileName(window, 'Open file',
                 '',"")
-            path = self.attack_file.rfind('/')
-            self.attack_file = self.attack_file[path+1:]
-            lbl_loaded.setText(self.attack_file)
-            script_textBox.setText("")
 
-            with open(self.attack_file) as f:
-                for line in f.readlines():
-                    self.script += line
-                    script_textBox.appendText(line)
+            if self.attack_file != "":
+                path = self.attack_file.rfind('/')
+                self.attack_file = self.attack_file[path+1:]
+                lbl_loaded.setText(self.attack_file)
+                self.script_textBox.setText("")
 
-            script_textBox.updateText()
+                with open(self.attack_file) as f:
+                    for line in f.readlines():
+                        self.script_textBox.appendText(line)
+
+                self.script_textBox.updateText()
 
         btn_load.clicked.connect(on_click)
 
         @pyqtSlot()
         def on_click():
-            self.start()
+            self.attack()
 
         btn_att.clicked.connect(on_click)
 
@@ -111,7 +126,11 @@ class Module_Run_Script(QThread):
         self.new_textBox = new_textBox
 
     def attack(self):
-        self.start()
+        #print(self.script_textBox.toPlainText())
+        for line in self.script_textBox.toPlainText().splitlines():
+            #print("running " + line[0:15])
+            runner = Run_Script(line)
+            runner.start()
 
-    def run(self):
-        exec("if " + self.script)
+    def __del__(self):
+        self.wait()
